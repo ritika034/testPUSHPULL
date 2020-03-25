9cdf30d92b0f61fc2ae6c583f4f295d5efa5bfd5
ritika034 <44552570+ritika034@users.noreply.github.com>
2020-03-25 14:50:39 +0530

    Adding content on github

--- testJavaProject1/src/testJavaProject1/testClass2.java
+++ testJavaProject1/src/testJavaProject1/testClass2.java
@@ -4,5 +4,6 @@ public class testClass2 {
  void testClass2(int a,int b)
  {
   System.out.println("Hey! I am a constructor of testClass2");
+  System.out.println("Hey! I am a constructor of testClass2,changed on github");
  }
 }
